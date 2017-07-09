f4c2a39760597ada0dcc6929335c6c163fab3ecd
akhilcjacob <akhilcjacob@gmail.com>
2017-07-09 18:11:39 -0400

    calculator is now functional

--- app/src/main/java/com/example/akhil/simplecalculator/MainActivity.java
+++ app/src/main/java/com/example/akhil/simplecalculator/MainActivity.java
@@ -1,13 +1,67 @@
 package com.example.akhil.simplecalculator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import bsh.EvalError;
+import bsh.Interpreter;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView outputBox;
+    private TextView computeBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        outputBox = (TextView) findViewById(R.id.outputText);
+        computeBox = (TextView) findViewById(R.id.computeText);
+    }
+
+    public void onClick(View view) {
+        Button clickedButton = (Button) view;
+        String clickedButtonName = clickedButton.getText().toString();
+        if (clickedButtonName.equals(outputBox.getText().toString().substring(outputBox.getText().toString().length() - 2, outputBox.getText().toString().length() - 1))) {
+            return;
+        }
+        Set<String> names = new HashSet<>();
+        names.add(getString(R.string.multiplyString));
+        names.add(getString(R.string.minusString));
+        names.add(getString(R.string.plusString));
+        if (names.contains(clickedButtonName)) {
+            outputBox.setText(outputBox.getText() + " " + clickedButtonName + " ");
+        } else {
+            outputBox.setText(outputBox.getText() + "" + clickedButtonName);
+        }
+        if (!outputBox.getText().equals("  ")) {
+            compute(view);
+        }
+    }
+
+    public void deleteLastChar(View view) {
+        if (outputBox.getText().equals("  ")) {
+            return;
+        }
+        String deleteLastChar = outputBox.getText().toString().substring(0, outputBox.getText().toString().length() - 1);
+        outputBox.setText(deleteLastChar);
+    }
+
+    public void compute(View view) {
+        System.out.println("Equasdlkfjalskdjflkasjdf;ljasd;lfdj;lasdjkf");
+        Interpreter interpreter = new Interpreter();
+        try {
+            interpreter.eval("result=" + outputBox.getText().toString());
+            String result = interpreter.get("result").toString();
+            System.out.println(result);
+            computeBox.setText(result);
+        } catch (EvalError evalError) {
+            evalError.printStackTrace();
+        }
     }
 }
