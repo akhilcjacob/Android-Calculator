c7e4e591653cf00fcf7ec6d674168966e4b4bd7e
akhilcjacob <akhilcjacob@gmail.com>
2017-07-10 20:29:13 -0400

    Added ability to disable continuous computing, Provided option to copy/paste output and input

--- app/src/main/java/com/example/akhil/simplecalculator/MainActivity.java
+++ app/src/main/java/com/example/akhil/simplecalculator/MainActivity.java
@@ -4,64 +4,121 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
-
+import bsh.EvalError;
+import bsh.Interpreter;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
-import bsh.EvalError;
-import bsh.Interpreter;
-
 public class MainActivity extends AppCompatActivity {
-    private TextView outputBox;
-    private TextView computeBox;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        outputBox = (TextView) findViewById(R.id.outputText);
-        computeBox = (TextView) findViewById(R.id.computeText);
+  private EditText userInputBox;
+  private TextView computeBox;
+  private int currentComputeNum;
+  private boolean continuousCompute;
+  private ArrayList<String> previousCompute;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    currentComputeNum = 0;
+    continuousCompute = false;
+    previousCompute = new ArrayList<>();
+    previousCompute.add("  ");
+    userInputBox = (EditText) findViewById(R.id.userInputBox);
+    computeBox = (TextView) findViewById(R.id.computeText);
+
+    Button deleteButton = (Button) findViewById(R.id.deleteButton);
+    deleteButton.setOnLongClickListener(new View.OnLongClickListener() {
+      @Override
+      public boolean onLongClick(View view) {
+        userInputBox.setText("");
+        computeBox.setText("");
+        return false;
+      }
+    });
+    if (!continuousCompute) {
+      computeBox.setVisibility(View.GONE);
+      findViewById(R.id.divider).setVisibility(View.GONE);
+    }
+  }
+
+  public void onNumberClick(View view) {
+    Button clickedButton = (Button) view;
+    String clickedButtonName = clickedButton.getText().toString();
+
+    if (userInputBox.getText().length() > 2 && clickedButtonName
+        .equals(userInputBox.getText().toString()
+            .substring(userInputBox.getText().toString().length() - 2,
+                userInputBox.getText().toString().length() - 1))) {
+      return;
+    }
+    Set<String> names = new HashSet<>();
+    names.add(getString(R.string.multiplyString));
+    names.add(getString(R.string.minusString));
+    names.add(getString(R.string.plusString));
+    names.add(getString(R.string.divideString));
+    if (names.contains(clickedButtonName)) {
+      userInputBox.setText(userInputBox.getText() + " " + clickedButtonName + " ");
+    } else {
+      userInputBox.setText(userInputBox.getText() + "" + clickedButtonName);
+    }
+    if (!userInputBox.getText().equals("") && continuousCompute) {
+      compute();
+    }
+  }
+
+  public void deleteLastChar(View view) {
+    if (userInputBox.getText().length() == 0) {
+      return;
     }
 
-    public void onClick(View view) {
-        Button clickedButton = (Button) view;
-        String clickedButtonName = clickedButton.getText().toString();
-        if (clickedButtonName.equals(outputBox.getText().toString().substring(outputBox.getText().toString().length() - 2, outputBox.getText().toString().length() - 1))) {
-            return;
-        }
-        Set<String> names = new HashSet<>();
-        names.add(getString(R.string.multiplyString));
-        names.add(getString(R.string.minusString));
-        names.add(getString(R.string.plusString));
-        if (names.contains(clickedButtonName)) {
-            outputBox.setText(outputBox.getText() + " " + clickedButtonName + " ");
-        } else {
-            outputBox.setText(outputBox.getText() + "" + clickedButtonName);
-        }
-        if (!outputBox.getText().equals("  ")) {
-            compute(view);
-        }
+    char lastChar = userInputBox.getText().toString().toCharArray()[
+        userInputBox.getText().length() - 1];
+    if (lastChar == ' ') {
+      userInputBox.getText().delete(userInputBox.getText().length() - 2,
+          userInputBox.getText().length());
     }
+    userInputBox.getText().delete(userInputBox.getText().length() - 1,
+        userInputBox.getText().length());
+  }
 
-    public void deleteLastChar(View view) {
-        if (outputBox.getText().equals("  ")) {
-            return;
-        }
-        String deleteLastChar = outputBox.getText().toString().substring(0, outputBox.getText().toString().length() - 1);
-        outputBox.setText(deleteLastChar);
-    }
+  public void equalSignClick(View view) {
+    compute();
+    currentComputeNum++;
+    previousCompute.add(userInputBox.getText().toString());
+    userInputBox.setText(computeBox.getText());
+    computeBox.setText("");
+  }
 
-    public void compute(View view) {
-        System.out.println("Equasdlkfjalskdjflkasjdf;ljasd;lfdj;lasdjkf");
-        Interpreter interpreter = new Interpreter();
-        try {
-            interpreter.eval("result=" + outputBox.getText().toString());
-            String result = interpreter.get("result").toString();
-            System.out.println(result);
-            computeBox.setText(result);
-        } catch (EvalError evalError) {
-            evalError.printStackTrace();
-        }
+
+  private void compute() {
+    try {
+      Interpreter interpreter = new Interpreter();
+      interpreter.eval("result=(float)" + userInputBox.getText().toString());
+      String result = interpreter.get("result").toString();
+      computeBox.setText(result);
+    } catch (EvalError evalError) {
+      evalError.printStackTrace();
     }
+  }
+
+  public void setNextCompute(View view) {
+    currentComputeNum++;
+    if (currentComputeNum >= previousCompute.size()) {
+      currentComputeNum = previousCompute.size() - 1;
+    }
+    userInputBox.setText(previousCompute.get(currentComputeNum));
+  }
+
+  public void setPreviousCompute(View view) {
+    currentComputeNum--;
+    if (currentComputeNum < 0) {
+      currentComputeNum = 0;
+    }
+    userInputBox.setText(previousCompute.get(currentComputeNum));
+  }
 }
